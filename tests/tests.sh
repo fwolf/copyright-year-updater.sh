63f0b7ab53c7cb0308e5f399dafe09bcd31e9ae2
Fwolf <fwolf.aide+git@gmail.com>
2015-03-16 14:59:38 +0800

    Add more test of UpdateLine function

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,11 +7,46 @@ cd "${P2T}/"
 PROGRAM=../copyright-year-updater.sh
 
 source assert.sh
+source ../lib/update-file.sh
 
 
 # Print usage
 assert_raises "$PROGRAM" 1
 assert_end "PrintUsage"
+
+
+# UpdateLine
+CURRENT_YEAR=2015
+# Disable file expansion, see https://stackoverflow.com/a/104023/1759745
+set -f
+# Disable word splitting, see https://www.gnu.org/software/bash/manual/bashref.html#Word-Splitting
+IFS=""
+
+x=" * @copyright   Copyright 2013 Holder"
+y=" * @copyright   Copyright 2013, 2015 Holder"
+assert "UpdateLine \"$x\"" "$y"
+
+x=" * @copyright   Copyright (c) 2013 Holder"
+y=" * @copyright   Copyright (c) 2013, 2015 Holder"
+assert "UpdateLine \"$x\"" "$y"
+
+x=" * @copyright   Copyright (C) 2013 Holder"
+y=" * @copyright   Copyright (C) 2013, 2015 Holder"
+assert "UpdateLine \"$x\"" "$y"
+
+x=" * @copyright   COPYRIGHT &copy; 2013 Holder"
+y=" * @copyright   COPYRIGHT &copy; 2013, 2015 Holder"
+assert "UpdateLine \"$x\"" "$y"
+
+x=" * @copyright   Copyright © 2013 Holder"
+y=" * @copyright   Copyright © 2013, 2015 Holder"
+assert "UpdateLine \"$x\"" "$y"
+
+# Holder must not be empty
+x=" * @copyright   Copyright 2013"
+assert "UpdateLine \"$x\"" ""
+
+assert_end "UpdateLine"
 
 
 # Fully run
