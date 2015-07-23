4f83164075f47b3f15a3a3866708ab1b5599eefc
Fwolf <fwolf.aide+git@gmail.com>
2015-07-23 16:01:03 +0800

    New git pre-commit hook example

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Build Status](https://travis-ci.org/fwolf/copyright-year-updater.sh.svg?branch=master)](https://travis-ci.org/fwolf/copyright-year-updater.sh)
 
-Automatic update year in copyright notice, follow 
+Automatic update year in copyright notice, follow
 [GNU Suggestion](https://www.gnu.org/licenses/gpl-howto.html).
 
 
@@ -18,13 +18,20 @@ Automatic update year in copyright notice, follow
 
 All options must set before `[File]`.
 
-    
+
 ### Multiple files
 
 Currently can only update single file, but we can use `find` and `xargs`  to
 treat multiple files:
 
     find *.sh | xargs -L1 copyright-year-updater.sh
+
+
+## Git pre-commit hook
+
+Copy `pre-commit.sample.sh` as `.git/hooks/pre-commit`, chmod +x.
+
+This hook will not touch un-staged file, even this file have staged part.
 
 
 ## License
---
+++ pre-commit.sample.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#====================================================================
+# Git pre commit hook to update copyright year
+#
+# Copy this file as '.git/hooks/pre-commit', chmod +x.
+#
+# @see https://stackoverflow.com/questions/22858850
+#====================================================================
+
+
+export COPYRIGHT_YEAR_UPDATER='copyright-year-updater.sh'
+
+
+git diff-index --cached --name-only HEAD | xargs -I % sh -c '
+    git ls-files --stage % | while read MODE OBJECT STAGE FILE_PATH; do
+        case $MODE in
+        10*)
+            # Copy file to temporary
+            STAGED_FILE=$(mktemp)
+            git show $OBJECT > "$STAGED_FILE"
+
+            # Do change copyright year
+            FORMATTED_FILE=$(mktemp)
+            cp "$STAGED_FILE" "$FORMATTED_FILE"
+            $COPYRIGHT_YEAR_UPDATER "$FORMATTED_FILE"
+
+            # Write new file blob to object database
+            FORMATTED_HASH=`git hash-object -w "$FORMATTED_FILE"`
+
+            # Register new written file to working tree index
+            git update-index --cacheinfo $MODE $FORMATTED_HASH "$FILE_PATH"
+
+            # Patch file in workspace, make it seems changed too
+            diff "$STAGED_FILE" "$FORMATTED_FILE" | patch "$FILE_PATH"
+
+            rm "$STAGED_FILE"
+            rm "$FORMATTED_FILE"
+        ;;
+        esac
+    done
+'
+
+
+exit 0
